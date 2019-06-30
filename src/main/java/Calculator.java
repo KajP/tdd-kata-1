342953b77d67c5bec6d1e3cd9dcc5cd7f67407ac
Kaj Pityński <210035@student.pwr.edu.pl>
2019-06-30 15:27:28 +0200

    Task 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,14 @@
 public class Calculator {
     public static int add(String input) {
-        return 0;
+        int index = input.indexOf(",");
+        int sum = 0;
+        if (input.length() == 0)
+            return sum;
+        if (index >= 0) {
+            int first = Integer.parseInt(input.substring(0, index)),
+                    second = Integer.parseInt(input.substring(index+1));
+            return first+second;
+        }
+        return Integer.parseInt(input);
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    @Test
+    void task1() {
+        assertEquals(0, Calculator.add(""));
+        assertEquals(1, Calculator.add("1"));
+        assertEquals(3, Calculator.add("1,2"));
+    }
 }
